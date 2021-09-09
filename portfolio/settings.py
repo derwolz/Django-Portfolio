f9032a8b01d9e30f1b99ab68e6c4c92b34f4dd25
unknown <der.wolz@gmail.com>
2021-09-09 00:18:40 -0700

    Mobile UI fixes and locally deployable site

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import json
-with open('etc/config.json') as config_file:
+with open('./etc/config.json') as config_file:
     config = json.load(config_file)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +28,7 @@ SECRET_KEY = config["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.236', '127.0.0.1', 'blainewolz.ddns.net']
+ALLOWED_HOSTS = ['localhost', '192.168.0.236', '127.0.0.1', 'blainewolz.ddns.net']
 
 
 # Application definition
